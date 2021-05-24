fb24d1a31e243aa40af470c220e314b3c869f09d
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-05-24 22:24:56 +0200

    send draft

--- client/requests/send.c
+++ client/requests/send.c
@@ -10,7 +10,9 @@
 request_t send_req(char **user_req, use_level_t *context_level)
 {
     request_t new_req;
-
+    strcpy(new_req.uuid, user_req[1]);
+    strcpy(new_req.message, user_req[2]);
     new_req.type = SEND;
+
     return new_req;
 }
