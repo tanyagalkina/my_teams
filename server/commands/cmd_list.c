ab253ee07d3feaa869754b96cc24f43803868d8e
marcel <marcel.taubert@epitech.eu>
2021-05-30 18:45:16 +0200

    add status response for /list teams

--- server/commands/cmd_list.c
+++ server/commands/cmd_list.c
@@ -13,12 +13,39 @@
 #include <stdio.h>
 #include <sys/queue.h>
 
+static int how_many_teams(server_t *server)
+{
+    int counter = 0;
+    team_t *team;
+
+    TAILQ_FOREACH(team, &server->admin->team_head, next) {
+        counter++;
+    }
+    return counter;
+}
+
+static void send_status(server_t *server, user_t *user)
+{
+    response_t r;
+    user_fds_t *fds;
+
+    r.request_type = CT_LIST;
+    r.status_code = STATUS_OK;
+    r.extern_body_size = how_many_teams(server);
+    r.extern_body_type = TEAM_TYPE;
+
+    TAILQ_FOREACH(fds, &user->user_fds_head, next) {
+        send(fds->fd, &r, RESPONSE_SIZE, 0);
+    }
+}
+
 static void list_teams(server_t *server, int fd)
 {
     response_t r;
     team_t *team = NULL;
     user_fds_t *fds;
     user_t *user = get_user_by_fd(server, fd);
+    send_status(server, user);
 
     TAILQ_FOREACH(team, &server->admin->team_head, next) {
         r.request_type = CT_LIST;
